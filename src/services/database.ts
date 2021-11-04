45397fb0dede6580fb5fb3f7e9fd7f15660c0a9b
victaphu <victaphu@gmail.com>
2021-11-04 14:29:36 +0800

    update: made it possible to load data from the environment

--- src/services/database.ts
+++ src/services/database.ts
@@ -9,7 +9,21 @@ export class DBService {
   constructor() {
     // Init admin
     try {
-      const serviceAccount = require('../../keys/keys.json');
+      const serviceAccount = {
+        type:process.env.type,
+        projectId:process.env.project_id,
+        privateKey:process.env.private_key.replace(/\\n/g, '\n'),
+        project_id:process.env.project_id,
+        private_key_id:process.env.private_key_id.replace(/\\n/g, '\n'),
+        private_key:process.env.private_key,
+        client_email:process.env.client_email,
+        client_id:process.env.client_id,
+        auth_uri:process.env.auth_uri,
+        token_uri:process.env.token_uri,
+        auth_provider_x509_cert_url:process.env.auth_provider_x509_cert_url,
+        client_x509_cert_url:process.env.client_x509_cert_url
+      };
+      //require('../../keys/keys.json');
 
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
