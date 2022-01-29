25ac5ce1cc248cf3a597daef4b5c377b10592aca
Victa Kwok Wai Phu <victa.phu@gmail.com>
2022-01-29 15:14:58 +1100

    update: added support for proxy implementations

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { asyncHandler } from "./helpers";
 import { IServices } from "../services/init";
 import codeVerication from "../services/codeverification";
 import { getAddress } from "@harmony-js/crypto";
+import { getProxyAddress } from "../services/utils";
 
 export const routes = (app, services: IServices) => {
   app.get(
@@ -53,12 +54,27 @@ export const routes = (app, services: IServices) => {
       ).checksum.toLowerCase();
 
       const result = await services.database.getContractCode(contractAddress);
-      const fileObj = await services.database.getContractSupportingFiles(contractAddress);
+
+      if (result) {
+        const fileObj = await services.database.getContractSupportingFiles(contractAddress);
+        result.supporting = fileObj;
+        try {
+          const proxy = await getProxyAddress(contractAddress);
+          result.proxyAddress = proxy;
+          if (proxy && proxy !== "") {
+            result.proxy = await services.database.getContractCode(proxy.toLocaleLowerCase());
+          }
+        }
+        catch (e) {
+          // no proxy address
+          console.error("No proxy", e);
+        }
+      }
 
       if (!result) {
         res.status(400).send({ message: "contract not found" });
+        return;
       }
-      result.supporting = fileObj;
 
       res.status(200).send(result);
     })
--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,5 +1,30 @@
 import { randomBytes } from '@harmony-js/crypto/dist/random';
+import { getBeaconAddress, getImplementationAddress, isBeaconProxy, isTransparentOrUUPSProxy } from "@openzeppelin/upgrades-core";
+
+import { ethers } from "ethers";
 
 export const uuidv4 = () => {
   return [randomBytes(4), randomBytes(4), randomBytes(4), randomBytes(4)].join('-');
 };
+
+export const getProxyAddress = async (address: string): Promise<string | null> => {
+  const web3URL = process.env.REACT_APP_RPC_URL_SHARD0;
+
+  // @ts-ignore
+  const provider = new ethers.providers.JsonRpcProvider(web3URL);
+  // /const provider = hmyWeb3.givenProvider;
+
+  console.log("is proxy?", address);
+  if (await isBeaconProxy(provider, address)) {
+    console.log("Is Beacon Proxy");
+    return await getBeaconAddress(provider, address);
+  }
+  else if (await isTransparentOrUUPSProxy(provider, address)) {
+    console.log("Is Transparent Proxy");
+    return await getImplementationAddress(provider, address);
+  }
+  else {
+    console.log("Nope, no idea what it is");
+    return null;
+  }
+}
